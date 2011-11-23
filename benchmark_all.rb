66c4f7c74342d483d8e2688d04aa826fe91f4593
Sean Reque <seanreque@yahoo.com>
2011-11-23 00:00:52 -0600

    added ikarus benchmark

--- benchmark_all.rb
+++ benchmark_all.rb
@@ -1,6 +1,26 @@
 FloatRegex = /[-+]?(?:\d+(?:\.\d*)?|\.\d+)|unknown/
 BenchmarkLineRegex = /^([^:]+): real=(#{FloatRegex})s\s+cpu=(#{FloatRegex})s\s+gc=(#{FloatRegex})s\s*$/
 require 'csv'
+
+def run_process_get_output(args, out_type=:out, &blk)
+  stdin_read, stdin_write = IO.pipe
+  pid = Process.spawn(*args, out_type => stdin_write)
+  result = [nil]
+  t = Thread.new do
+    result[0] = blk[stdin_read]
+  end
+
+  rc = Process.waitpid2(pid)[1].exitstatus
+  stdin_write.close
+  t.join
+  stdin_read.close
+
+  if rc != 0
+    raise "command failed with exit code#{rc}: #{args.join " "}"
+  end
+  return result[0]
+end
+
 class BenchmarkRunner
   attr_accessor :prog_path
 
@@ -15,9 +35,7 @@ class BenchmarkRunner
   def run(benchmark_dir)
     result = {}
     args = mk_cmd(benchmark_dir)
-    stdin_read, stdin_write = IO.pipe
-    pid = Process.spawn(*args, :out => stdin_write)
-    Thread.new do
+    run_process_get_output(args) do |stdin_read|
       stdin_read.each_line do |line|
         if line =~ BenchmarkLineRegex
           result[File.basename($1)] = [$2, $3, $4].map{|v| v == "unknown" ? "unknown" : Float(v) }
@@ -25,12 +43,6 @@ class BenchmarkRunner
           puts "unrecognized line: #{line}"
         end
       end
-    end
-
-    rc = Process.waitpid2(pid)[1].exitstatus
-
-    if rc != 0
-      raise "command failed with exit code#{rc}: #{args.join " "}"
     end
     return result
   end
@@ -65,6 +77,92 @@ class BiglooRunner < BenchmarkRunner
     %W!bigloo -i #{prog_path} #{benchmark_dir}!
   end
 end
+
+class IkarusRunner < BenchmarkRunner
+  def mk_cmd(benchmark_dir)
+    %W!ikarus --r6rs-script #{prog_path} #{benchmark_dir}!
+  end
+
+  def run(benchmark_dir)
+    args = mk_cmd(benchmark_dir)
+    run_process_get_output(args, :err) do |stdin_read|
+      IkarusFsm.new(stdin_read).results
+    end
+  end
+end
+
+class IkarusFsm
+  attr_accessor :results
+  def initialize(input)
+    @input = input
+    self.results = {}
+    @next_func = :idle
+    parse()
+  end
+
+  def parse()
+    @input.each_line do |line|
+      @next_func = self.send(@next_func, line)
+    end
+  end
+
+  def idle(line)
+    if line =~ /^running stats for `([^`]+)`:\s*$/
+      @file, @real, @cpu, @gc = $1, nil, nil, nil
+       :expect_collections
+    else
+      unrecognized(line)
+    end
+  end
+
+  def expect_collections(line)
+    if line =~ /^    (?:\d+|no) collections/
+      :expect_cpu
+    else
+      unrecognized(line)
+    end
+  end
+
+  def expect_cpu(line)
+    if line =~ /^    (\d+) ms elapsed cpu time, including (\d+) ms collecting$/
+      @cpu = Float($1) / 1000
+      @gc = Float($2) / 1000
+      :expect_real
+    else
+      unrecognized(line)
+    end
+  end
+
+  def expect_real(line)
+    if line =~ /^    (\d+) ms elapsed real time, including (\d+) ms collecting$/
+      @real = Float($1) / 1000
+      @gc = Float($2) / 1000
+      :expect_allocated
+    else
+      unrecognized(line)
+    end
+  end
+
+  def expect_allocated(line)
+    if line =~ /^    (\d+) bytes allocated$/
+      self.results[File.basename(@file)] = [@real, @cpu, @gc]
+      go_idle
+    else
+      unrecognized(line)
+    end
+  end
+
+  def unrecognized(line)
+    puts "unrecognized line: #{line}"
+    go_idle
+  end
+
+  def go_idle
+    @file, @real, @cpu, @gc = nil, nil, nil, nil
+    return :idle
+  end
+end
+
 if __FILE__ == $0
   script_dir = File.dirname(__FILE__)
   prog_dir = File.join(script_dir, 'benchmarkers')
@@ -75,6 +173,7 @@ if __FILE__ == $0
     PetiteRunner.new(File.join(prog_dir, 'chez.ss')),
     ChickenRunner.new(File.join(prog_dir, 'chicken.ss')),
     GambitRunner.new(File.join(prog_dir, 'gambit.ss')),
+    IkarusRunner.new(File.join(prog_dir, 'ikarus.ss')),
   ]
   all_benchmarks = %W!rkt scm ss!.map {|ext| Dir.glob("#{benchmark_dir}/**/*.#{ext}") }.flatten.map {|v| File.basename(v) }
   results = runners.map {|runner| [File.basename(runner.prog_path), runner.run(benchmark_dir)]}
---
+++ benchmarkers/ikarus.ss
@@ -0,0 +1,61 @@
+(import (rnrs) (ikarus))
+
+(define (errorf fmt . args)
+  (raise
+    (condition
+      (make-error)
+      (make-message-condition (apply format (cons fmt args))))))
+
+(define (string-ends-with? str sfx)
+  (define sfx-len (string-length sfx))
+  (define str-len (string-length str))
+  (if (< str-len sfx-len)
+    #f
+    (let loop ((idx (sub1 str-len)) 
+               (sfx-idx (sub1 sfx-len)))
+      (if (< sfx-idx 0)
+        #t
+        (if (not (char=? (string-ref str idx) (string-ref sfx sfx-idx)))
+          #f
+          (loop (sub1 idx) (sub1 sfx-idx)))))))
+
+(define (read-all path)
+  (define rev-result '())
+  (load path
+        (lambda (form)
+          (set! rev-result (cons (expand form) rev-result))))
+  (reverse rev-result))
+
+;(define (time-apply f)
+  ;(define start-time (statistics))
+  ;(define stat-time (statistics))
+  ;(define result (f))
+  ;(cons result (sstats-difference (sstats-difference  (statistics) start-time) (sstats-difference stat-time start-time))))
+;
+;(define (benchmark f)
+  ;(collect)
+  ;(time-apply f))
+
+(define args (cdr (command-line)))
+(when (not (eqv? 1 (length args)))
+  (errorf "expected one argument, got ~a: ~a" (length args) args))
+(define benchmark-dir (car args))
+(when (not (file-exists? benchmark-dir))
+  (errorf "provided path does not exist or is not a directory: ~a" benchmark-dir))
+(define src-files 
+  (map (lambda (v)
+         (string-append benchmark-dir "/" v))
+    (filter (lambda (v)
+              (or (string-ends-with? v ".rkt") (string-ends-with? v ".scm")))
+            (directory-list benchmark-dir))))
+
+;(display (read-all (car src-files)))
+(let loop ((rem src-files))
+  (when (not (null? rem))
+    (let* ((path (car rem))
+           (result (time-it (format "`~a`" path) (lambda () (read-all path))))
+           (stats (cdr result))
+           (expanded (car result)))
+      ;(printf "~a\n" expanded)
+      ;(printf "~a: real=~as cpu=~as gc=~as\n" path (/ (sstats-real stats) 1000.0) (/ (sstats-cpu stats) 1000.0) (/ (sstats-gc-real stats) 1000.0))
+      (loop (cdr rem)))))
